6ff22416cc48a7ec3ca9ba3207b5bd4656450bd7
405102091 <405102091@users.noreply.github.com>
2017-04-09 15:57:08 +0800

    Update startup.sh

--- my_gj/chrome/startup.sh
+++ my_gj/chrome/startup.sh
@@ -6,5 +6,6 @@ mkdir -p /var/run/sshd
 # start up supervisord, all daemons should launched by supervisord.
 /usr/bin/supervisord -c /root/supervisord.conf
 
+tmux new-session -d "python3 /root/ameblocal.py 2>&1"
 # start a shell
 /bin/bash
